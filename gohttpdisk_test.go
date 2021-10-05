17b95edd9b5e62a5e6ff334bece2d1282e26b85c
Nathan Kriege <nkriege@gmail.com>
2021-10-05 14:01:57 -0500

    don't hit network during tests

--- gohttpdisk_test.go
+++ gohttpdisk_test.go
@@ -1,21 +1,22 @@
 package gohttpdisk
 
 import (
+	"errors"
+	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"sync"
 	"testing"
 	"time"
 
+	"github.com/dnaeon/go-vcr/recorder"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestHTTPDisk(t *testing.T) {
-	hd := NewHTTPDisk(Options{Dir: TmpDir()})
-	hd.Cache.RemoveAll()
-	defer hd.Cache.RemoveAll()
-
-	client := http.Client{Transport: hd}
+	client := setupClient(t, Options{})
+	defer teardownClient(client)
 
 	drainBody := func(resp *http.Response) string {
 		data, err := ioutil.ReadAll(resp.Body)
@@ -29,17 +30,14 @@ func TestHTTPDisk(t *testing.T) {
 	// 1. miss
 	//
 
-	url := "http://httpbingo.org/get"
+	url := "http://example.com/get"
 	resp, err := client.Get(url)
 	if err != nil {
 		t.Fatalf("Get %s failed %s", url, err)
 	}
 	defer resp.Body.Close()
-	body1 := drainBody(resp)
-	date1 := resp.Header.Get("Date")
-	id1 := resp.Header.Get("fly-request-id")
-	assert.NotNil(t, date1)
-	assert.NotNil(t, id1)
+	assert.Equal(t, "body 1", drainBody(resp))
+	assert.Equal(t, "1", resp.Header.Get("X-Request-Id"))
 
 	//
 	// 2. hit
@@ -50,30 +48,25 @@ func TestHTTPDisk(t *testing.T) {
 		t.Fatalf("Get %s failed %s", url, err)
 	}
 	defer resp.Body.Close()
-	assert.Equal(t, body1, drainBody(resp))
-	assert.Equal(t, date1, resp.Header.Get("Date"))
-	assert.Equal(t, id1, resp.Header.Get("fly-request-id"))
+	assert.Equal(t, "body 1", drainBody(resp))
+	assert.Equal(t, "1", resp.Header.Get("X-Request-Id"))
 }
 
 func TestHTTPDiskForce(t *testing.T) {
-	hd := NewHTTPDisk(Options{Dir: TmpDir(), Force: true})
-	hd.Cache.RemoveAll()
-	defer hd.Cache.RemoveAll()
-
-	client := http.Client{Transport: hd}
+	client := setupClient(t, Options{Force: true})
+	defer teardownClient(client)
 
 	//
 	// 1. miss
 	//
 
-	url := "http://httpbingo.org/get"
+	url := "http://example.com/get"
 	resp, err := client.Get(url)
 	if err != nil {
 		t.Fatalf("Get %s failed %s", url, err)
 	}
 	defer resp.Body.Close()
-	id1 := resp.Header.Get("fly-request-id")
-	assert.NotNil(t, id1)
+	assert.Equal(t, "1", resp.Header.Get("X-Request-Id"))
 
 	//
 	// 2. force second request
@@ -84,117 +77,168 @@ func TestHTTPDiskForce(t *testing.T) {
 		t.Fatalf("Get %s failed %s", url, err)
 	}
 	defer resp.Body.Close()
-	assert.NotEqual(t, id1, resp.Header.Get("fly-request-id"))
+	assert.Equal(t, "2", resp.Header.Get("X-Request-Id"))
 }
 
 func TestHTTPDiskErrors(t *testing.T) {
+	client := setupClient(t, Options{})
+	defer teardownClient(client)
+
+	var resp *http.Response
+	var err error
+
+	// Nework errors are tested elsewhere. See TestHTTPDiskTimeout and TestHTTPDiskNoSuchHost.
+
+	// 40x error
+	url := "http://httpbingo.org/status/404"
+	resp, err = client.Get(url)
+	assert.Nil(t, err)
+	assert.Equal(t, 404, resp.StatusCode)
+	assert.Equal(t, "1", resp.Header.Get("X-Request-Id"))
+
+	resp, err = client.Get(url)
+	assert.Nil(t, err)
+	assert.Equal(t, 404, resp.StatusCode)
+	assert.Equal(t, "1", resp.Header.Get("X-Request-Id"), "response not cached")
+
+	// 50x error
+	url = "http://httpbingo.org/status/502"
+	resp, err = client.Get(url)
+	assert.Nil(t, err)
+	assert.Equal(t, 502, resp.StatusCode)
+	assert.Equal(t, "2", resp.Header.Get("X-Request-Id"))
+
+	resp, err = client.Get(url)
+	assert.Nil(t, err)
+	assert.Equal(t, 502, resp.StatusCode)
+	assert.Equal(t, "2", resp.Header.Get("X-Request-Id"), "response not cached")
+}
+
+func TestHTTPDiskTimeout(t *testing.T) {
+	// This test does not use vcr
 	hd := NewHTTPDisk(Options{Dir: TmpDir()})
 	hd.Cache.RemoveAll()
 	defer hd.Cache.RemoveAll()
 
-	var resp *http.Response
-	var err error
+	// Fake the error unless we're really hitting the network
+	if os.Getenv("USE_NETWORK") == "" {
+		hd.Transport = &errorRoundTripper{"context deadline exceeded"}
+	}
 
-	client := http.Client{Transport: hd, Timeout: time.Millisecond * 500}
+	client := http.Client{Transport: hd, Timeout: 500 * time.Millisecond}
 
-	// bad host
+	url := "http://httpbingo.org/delay/1"
+	_, err := client.Get(url)
+	assert.NotNil(t, err)
+
+	_, err = client.Get(url)
+	if assert.NotNil(t, err) {
+		assert.Contains(t, err.Error(), "(cached)", "%s error was not cached", url)
+	}
+}
+
+func TestHTTPDiskNoSuchHost(t *testing.T) {
+	// This test does not use vcr
+	hd := NewHTTPDisk(Options{Dir: TmpDir()})
+	hd.Cache.RemoveAll()
+	defer hd.Cache.RemoveAll()
+
+	// Fake the error unless we're really hitting the network
+	if os.Getenv("USE_NETWORK") == "" {
+		hd.Transport = &errorRoundTripper{"no such host"}
+	}
+
+	client := http.Client{Transport: hd}
+
 	url := "http://bogus.bogus"
-	client.Get(url)
+	_, err := client.Get(url)
+	assert.NotNil(t, err)
+
 	_, err = client.Get(url)
-	assert.Contains(t, err.Error(), "(cached)", "%s error was not cached", url)
-
-	// timeout
-	url = "http://httpbingo.org/delay/1"
-	client.Get(url)
-	_, err = client.Get(url)
-	assert.Contains(t, err.Error(), "(cached)", "%s error was not cached", url)
-
-	// 40x error
-	url = "http://httpbingo.org/status/404"
-	client.Get(url)
-	resp, err = client.Get(url)
-	assert.Nil(t, err)
-	assert.Equal(t, 404, resp.StatusCode)
-	id := resp.Header.Get("fly-request-id")
-	assert.NotNil(t, id)
-
-	resp, err = client.Get(url)
-	assert.Nil(t, err)
-	assert.Equal(t, 404, resp.StatusCode)
-	assert.Equal(t, id, resp.Header.Get("fly-request-id"), "response not cached")
-
-	// 50x error
-	url = "http://httpbingo.org/status/502"
-	client.Get(url)
-	resp, err = client.Get(url)
-	assert.Nil(t, err)
-	assert.Equal(t, 502, resp.StatusCode)
-	id = resp.Header.Get("fly-request-id")
-	assert.NotNil(t, id)
-
-	resp, err = client.Get(url)
-	assert.Nil(t, err)
-	assert.Equal(t, 502, resp.StatusCode)
-	assert.Equal(t, id, resp.Header.Get("fly-request-id"), "response not cached")
+	if assert.NotNil(t, err) {
+		assert.Contains(t, err.Error(), "(cached)", "%s error was not cached", url)
+	}
 }
 
 func TestHTTPDiskForceErrors(t *testing.T) {
+	client := setupClient(t, Options{ForceErrors: true})
+	defer teardownClient(client)
+
+	var resp *http.Response
+	var err error
+
+	// Nework errors are tested elsewhere. See TestHTTPDiskForceTimeout and TestHTTPDiskForceNoSuchHost.
+
+	// 40x error
+	url := "http://httpbingo.org/status/404"
+	resp, err = client.Get(url)
+	assert.Nil(t, err)
+	assert.Equal(t, 404, resp.StatusCode)
+	assert.Equal(t, "1", resp.Header.Get("X-Request-Id"))
+
+	resp, err = client.Get(url)
+	assert.Nil(t, err)
+	assert.Equal(t, 404, resp.StatusCode)
+	assert.Equal(t, "2", resp.Header.Get("X-Request-Id"), "response cached")
+
+	// 50x error
+	url = "http://httpbingo.org/status/502"
+	resp, err = client.Get(url)
+	assert.Nil(t, err)
+	assert.Equal(t, 502, resp.StatusCode)
+	assert.Equal(t, "3", resp.Header.Get("X-Request-Id"))
+
+	resp, err = client.Get(url)
+	assert.Nil(t, err)
+	assert.Equal(t, 502, resp.StatusCode)
+	assert.Equal(t, "4", resp.Header.Get("X-Request-Id"), "response cached")
+}
+
+func TestHTTPDiskForceTimeout(t *testing.T) {
+	// This test does not use vcr
 	hd := NewHTTPDisk(Options{Dir: TmpDir(), ForceErrors: true})
 	hd.Cache.RemoveAll()
 	defer hd.Cache.RemoveAll()
 
-	var resp *http.Response
-	var err error
+	// Fake the error unless we're really hitting the network
+	if os.Getenv("USE_NETWORK") == "" {
+		hd.Transport = &errorRoundTripper{"context deadline exceeded"}
+	}
 
-	client := http.Client{Transport: hd, Timeout: time.Millisecond * 500}
+	client := http.Client{Transport: hd, Timeout: 500 * time.Millisecond}
 
-	// bad host
-	url := "http://bogus.bogus"
+	url := "http://httpbingo.org/delay/1"
 	client.Get(url)
-	_, err = client.Get(url)
-	assert.NotContains(t, err.Error(), "(cached)", "%s ForceErrors not honored", url)
-
-	// timeout
-	url = "http://httpbingo.org/delay/1"
-	client.Get(url)
-	_, err = client.Get(url)
-	assert.NotContains(t, err.Error(), "(cached)", "%s ForceErrors not honored", url)
-
-	// 40x error
-	url = "http://httpbingo.org/status/404"
-	client.Get(url)
-	resp, err = client.Get(url)
-	assert.Nil(t, err)
-	assert.Equal(t, 404, resp.StatusCode)
-	id := resp.Header.Get("fly-request-id")
-	assert.NotNil(t, id)
-
-	resp, err = client.Get(url)
-	assert.Nil(t, err)
-	assert.Equal(t, 404, resp.StatusCode)
-	assert.NotEqual(t, id, resp.Header.Get("fly-request-id"), "response cached")
-
-	// 50x error
-	url = "http://httpbingo.org/status/502"
-	client.Get(url)
-	resp, err = client.Get(url)
-	assert.Nil(t, err)
-	assert.Equal(t, 502, resp.StatusCode)
-	id = resp.Header.Get("fly-request-id")
-	assert.NotNil(t, id)
-
-	resp, err = client.Get(url)
-	assert.Nil(t, err)
-	assert.Equal(t, 502, resp.StatusCode)
-	assert.NotEqual(t, id, resp.Header.Get("fly-request-id"), "response cached")
+	_, err := client.Get(url)
+	if assert.NotNil(t, err) {
+		assert.NotContains(t, err.Error(), "(cached)", "%s ForceErrors not honored", url)
+	}
 }
 
-func TestHTTPDiskExpires(t *testing.T) {
-	hd := NewHTTPDisk(Options{Dir: TmpDir(), Expires: 2 * time.Second})
+func TestHTTPDiskForceNoSuchHost(t *testing.T) {
+	// This test does not use vcr
+	hd := NewHTTPDisk(Options{Dir: TmpDir(), ForceErrors: true})
 	hd.Cache.RemoveAll()
 	defer hd.Cache.RemoveAll()
 
+	// Fake the error unless we're really hitting the network
+	if os.Getenv("USE_NETWORK") == "" {
+		hd.Transport = &errorRoundTripper{"no such host"}
+	}
+
 	client := http.Client{Transport: hd}
+
+	url := "http://bogus.bogus"
+	client.Get(url)
+	_, err := client.Get(url)
+	if assert.NotNil(t, err) {
+		assert.NotContains(t, err.Error(), "(cached)", "%s ForceErrors not honored", url)
+	}
+}
+
+func TestHTTPDiskExpires(t *testing.T) {
+	client := setupClient(t, Options{Expires: 100 * time.Millisecond})
+	defer teardownClient(client)
 
 	//
 	// 1. miss
@@ -206,8 +250,7 @@ func TestHTTPDiskExpires(t *testing.T) {
 		t.Fatalf("Get %s failed %s", url, err)
 	}
 	defer resp.Body.Close()
-	id1 := resp.Header.Get("fly-request-id")
-	assert.NotNil(t, id1)
+	assert.Equal(t, "1", resp.Header.Get("X-Request-Id"))
 
 	//
 	// 2. hit
@@ -218,30 +261,27 @@ func TestHTTPDiskExpires(t *testing.T) {
 		t.Fatalf("Get %s failed %s", url, err)
 	}
 	defer resp.Body.Close()
-	assert.Equal(t, id1, resp.Header.Get("fly-request-id"))
+	assert.Equal(t, "1", resp.Header.Get("X-Request-Id"))
 
 	//
 	// 3. expired
 	//
 
-	time.Sleep(3 * time.Second)
+	time.Sleep(150 * time.Millisecond)
 
 	resp, err = client.Get(url)
 	if err != nil {
 		t.Fatalf("Get %s failed %s", url, err)
 	}
 	defer resp.Body.Close()
-	assert.NotEqual(t, id1, resp.Header.Get("fly-request-id"))
+	assert.Equal(t, "2", resp.Header.Get("X-Request-Id"))
 }
 
 func TestHTTPDiskStaleWhileRevalidate(t *testing.T) {
 	var wg sync.WaitGroup
 
-	hd := NewHTTPDisk(Options{Dir: TmpDir(), Expires: 2 * time.Second, StaleWhileRevalidate: true, RevalidationWaitGroup: &wg})
-	hd.Cache.RemoveAll()
-	defer hd.Cache.RemoveAll()
-
-	client := http.Client{Transport: hd}
+	client := setupClient(t, Options{Expires: 100 * time.Millisecond, StaleWhileRevalidate: true, RevalidationWaitGroup: &wg})
+	defer teardownClient(client)
 
 	//
 	// 1. miss
@@ -253,8 +293,7 @@ func TestHTTPDiskStaleWhileRevalidate(t *testing.T) {
 		t.Fatalf("Get %s failed %s", url, err)
 	}
 	defer resp.Body.Close()
-	id1 := resp.Header.Get("fly-request-id")
-	assert.NotNil(t, id1)
+	assert.Equal(t, "1", resp.Header.Get("X-Request-Id"))
 
 	//
 	// 2. hit
@@ -264,21 +303,20 @@ func TestHTTPDiskStaleWhileRevalidate(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Get %s failed %s", url, err)
 	}
-	defer resp.Body.Close()
-	assert.Equal(t, id1, resp.Header.Get("fly-request-id"))
+	assert.Equal(t, "1", resp.Header.Get("X-Request-Id"))
 
 	//
 	// 3. stale
 	//
 
-	time.Sleep(3 * time.Second)
+	time.Sleep(150 * time.Millisecond)
 
 	resp, err = client.Get(url)
 	if err != nil {
 		t.Fatalf("Get %s failed %s", url, err)
 	}
 	defer resp.Body.Close()
-	assert.Equal(t, id1, resp.Header.Get("fly-request-id"))
+	assert.Equal(t, "1", resp.Header.Get("X-Request-Id"))
 
 	//
 	// 4. refreshed in background
@@ -292,7 +330,7 @@ func TestHTTPDiskStaleWhileRevalidate(t *testing.T) {
 		t.Fatalf("Get %s failed %s", url, err)
 	}
 	defer resp.Body.Close()
-	assert.NotEqual(t, id1, resp.Header.Get("fly-request-id"))
+	assert.Equal(t, "2", resp.Header.Get("X-Request-Id"))
 }
 
 func TestHTTPDiskStatus(t *testing.T) {
@@ -314,4 +352,66 @@ func TestHTTPDiskStatus(t *testing.T) {
 	MustWriteGzip(status.Path, "err:nope")
 	status, _ = hd.Status(req)
 	assert.Equal(t, "error", status.Status)
+}
+
+//
+// Helpers
+//
+
+// Create a new recorder for the current test.
+// Fixtures must be stored in fixtures/<testName>.yaml
+//
+// Callers are responsible for stopping the returned vcr. For example:
+//   vcr := newVCR(t)
+//   defer vcr.Stop()
+func newVCR(t *testing.T) *recorder.Recorder {
+	cassette := fmt.Sprintf("fixtures/%s", t.Name())
+	vcr, err := recorder.New(cassette)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return vcr
+}
+
+// Create and configure an http client for testing using httpdisk and recorder
+// for the current test.
+//
+// Fixtures must be stored in fixtures/<testName>.yaml
+//
+// Callers are responsible for tearing everything down at the end of the test.
+// For example:
+//   client := setupClient(t, options)
+//   defer teardownClient(client)
+func setupClient(t *testing.T, hdOptions Options) *http.Client {
+	// Default options
+	if hdOptions.Dir == "" {
+		hdOptions.Dir = TmpDir()
+	}
+
+	hd := NewHTTPDisk(hdOptions)
+	hd.Cache.RemoveAll()
+
+	vcr := newVCR(t)
+	hd.Transport = vcr
+
+	return &http.Client{Transport: hd}
+}
+
+func teardownClient(client *http.Client) {
+	hd := client.Transport.(*HTTPDisk)
+	hd.Cache.RemoveAll()
+
+	vcr := hd.Transport.(*recorder.Recorder)
+	vcr.Stop()
+}
+
+//
+// Custom RoundTripper that always returns an error
+//
+
+type errorRoundTripper struct{ errorString string }
+
+func (t *errorRoundTripper) RoundTrip(r *http.Request) (*http.Response, error) {
+	return nil, errors.New(t.errorString)
 }
